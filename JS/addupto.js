44569c1ae3ecf03dfd8a3fa13b4b4a248385de1d
Henry Neal <henry.neal0@gmail.com>
2019-12-11 11:14:55 -0500

    tweak addupto

--- JS/addupto.js
+++ JS/addupto.js
@@ -1,15 +1,13 @@
 function addUpTo(n) {
-    let total = 0;
-    for (let i = 1; i <= n; i++) {
-        total += i;
-    }
-    return total;
+	let total = 0;
+	for (let i = 1; i <= n; i++) {
+		total += i;
+	}
+	return total;
 }
 
-function addUpToBetter(n) {
-    return n * (n + 1) / 2
+function addUpToRevised(n) {
+	return (n * (n + 1)) / 2;
 }
 
-
-
-console.log(addUpTo(6))
+console.log(addUpTo(6));
