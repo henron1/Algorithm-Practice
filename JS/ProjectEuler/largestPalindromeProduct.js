4fcdd9399627aa19700bffea7204ac2874fb8930
Henry Neal <henry.neal0@gmail.com>
2020-05-28 12:48:22 -0400

    added largest palindrome

--- JS/ProjectEuler/largestPalindromeProduct.js
+++ JS/ProjectEuler/largestPalindromeProduct.js
@@ -1,5 +1,27 @@
+// 9009 = 91 x 99
+// find largest palindrome made from the product of two n-digit numbers
+
+// build function to check is palindrome
+
+
 function largestPalindromeProduct(n) {
-    largest
+    const palindromes = [];
+    const number = Array(n).fill(9).join('')
+
+    for (let i = number; i >= 1; i--) {
+        for (let j = number; j >= 1; j--) {
+            const result = i * j;
+            if (isPalindrome(result)) {
+                palindromes.push(result)
+            }
+        }
+    }
+    return Math.max(...palindromes)
 }
-largestPalindromeProduct(2)
-largestPalindromeProduct(3)
+
+function isPalindrome(number) {
+    return number.toString() === number.toString().split('').reverse().join('')
+}
+console.log(largestPalindromeProduct(2))
+console.log(largestPalindromeProduct(3))
+console.log(largestPalindromeProduct(4))
